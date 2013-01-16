aeb4e9d7c69d964908cd04e41fa12ecdcf124829
Ben Mather <bwhmather@gmail.com>
2013-01-16 18:14:42 +0000

    Start adding a couple of tests for ring middleware

---
+++ test/clj_oauth2/ring_test.clj
@@ -0,0 +1,51 @@
+(ns clj-oauth2.ring-test
+  (:use clojure.test
+        [clojure.data.json :only [json-str]]
+        [clojure.pprint :only [pprint]])
+  (:require [clj-oauth2.ring :as base]))
+
+
+
+(deftest request-uri
+    (testing
+      "does not write port if standard"
+      (is (= (base/request-uri {:scheme "http"
+                                :server-name "example.com"
+                                :server-port 80
+                                :uri "/"}
+                               {})
+             "http://example.com/")))
+    (testing
+      "uses default port if not specified"
+      (is (= (base/request-uri {:scheme "http"
+                                :server-name "example.com"
+                                :uri "/"}
+                               {})
+             "http://example.com/")))
+    (testing
+      "writes port if non-standard"
+      (is (= (base/request-uri {:scheme "http"
+                                :server-name "example.com"
+                                :server-port 1234
+                                :uri "/"}
+                               {})
+             "http://example.com:1234/")))
+    (testing
+      "forces https if set in params"
+      (is (= (base/request-uri {:scheme "http"
+                                :server-name "example.com"
+                                :server-port 80
+                                :uri "/"}
+                               {:force-https true})
+             "https://example.com/"))))
+
+(deftest is-callback
+    (testing
+      "is-callback returns true for valid urls"
+      (is (= (base/is-callback 
+               {}
+               {:redirect-uri "https://example.com/oauth/callback"}) true))
+      (is (= (base/is-callback "https://example.com/oauth/callback?param=something;") true)))
+    (testing
+      "is-callback returns false for invalid urls"
+      (is (= (base/is-callback "") false))))
